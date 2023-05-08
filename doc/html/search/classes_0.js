8ac07df898ef75cfffefe331510ee46605a1fae2
GeckoNickDeveloper <nicholas.nick.kania.development@gmail.com>
2023-05-08 18:36:37 +0200

    Documentation Update
    >>> Added basic doxygen doc build
    >>> TODO: add comment

---
+++ doc/html/search/classes_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['hmc5883l_5fconfig_5ft_0',['hmc5883l_config_t',['../structhmc5883l__config__t.html',1,'']]],
+  ['hmc5883l_5fdev_5ft_1',['hmc5883l_dev_t',['../structhmc5883l__dev__t.html',1,'']]]
+];
---
+++ doc/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['hmc5883l_20driver_20component_0',['HMC5883L Driver Component',['../md__r_e_a_d_m_e.html',1,'']]]
+];
